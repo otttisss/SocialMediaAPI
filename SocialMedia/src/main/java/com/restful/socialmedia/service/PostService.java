f8fca1bcdab91c7b69fcf43f4ce41d128014d253
otttisss <otttisss@yandex.ru>
2023-08-18 14:49:38 +0300

    PostService methods implements

--- SocialMedia/src/main/java/com/restful/socialmedia/service/PostService.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/service/PostService.java
@@ -1,7 +1,43 @@
 package com.restful.socialmedia.service;
 
+import com.restful.socialmedia.model.Post;
+import com.restful.socialmedia.model.User;
+import com.restful.socialmedia.repository.PostRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post createPost(User author, String title, String text) {
+        Post post = new Post();
+        post.setAuthor(author);
+        post.setTitle(title);
+        post.setText(text);
+
+        return postRepository.save(post);
+    }
+
+    public void updatePost(Long postId, String newTitle, String newText) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow( () -> new RuntimeException("Post not found")) ;
+        post.setTitle(newTitle);
+        post.setText(newText);
+
+        postRepository.save(post);
+    }
+
+    public void deletePost(Long postId) {
+        postRepository.deleteById(postId);
+    }
+
+    public List<Post> getPostByAuthor(User author) {
+        return postRepository.findByAuthor(author);
+    }
 }
