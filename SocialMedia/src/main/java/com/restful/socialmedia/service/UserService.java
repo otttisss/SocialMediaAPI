b45b1510626a51179561e117e74e1751bbaf3046
otttisss <otttisss@yandex.ru>
2023-08-18 14:17:29 +0300

    registerUser method implemented

--- SocialMedia/src/main/java/com/restful/socialmedia/service/UserService.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/service/UserService.java
@@ -1,7 +1,27 @@
 package com.restful.socialmedia.service;
 
+import com.restful.socialmedia.model.User;
+import com.restful.socialmedia.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(String username, String email, String password) {
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+
+        return userRepository.save(user);
+    }
 }
