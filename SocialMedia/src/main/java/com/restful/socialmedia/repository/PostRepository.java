ee521309297b6deec120a95f8452a1c82540d1d6
otttisss <otttisss@yandex.ru>
2023-08-18 14:43:05 +0300

    added more methods

--- SocialMedia/src/main/java/com/restful/socialmedia/repository/PostRepository.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/repository/PostRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.awt.print.Pageable;
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByAuthorIn(List<User> authors, Pageable pageable);
+    Optional<Post> findById(Long id);
+    List<Post> findByAuthor(User author);
+    void deleteById(Long id);
 }
