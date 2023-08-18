32678154c3777a9080f78a4925b3e9dee278102b
otttisss <otttisss@yandex.ru>
2023-08-18 14:07:58 +0300

    FriendRequest some fixes

--- SocialMedia/src/main/java/com/restful/socialmedia/model/FriendRequest.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/model/FriendRequest.java
@@ -8,8 +8,10 @@ public class FriendRequest {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
+    @JoinColumn(name = "sender_id")
     private User sender;
     @ManyToOne
+    @JoinColumn(name = "receiver_id")
     private User receiver;
 
     public Long getId() {
