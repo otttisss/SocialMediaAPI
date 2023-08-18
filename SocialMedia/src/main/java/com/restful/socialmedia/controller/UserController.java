d08b4d2246a9e697662892acf86d8b10a21865ec
otttisss <otttisss@yandex.ru>
2023-08-18 15:29:25 +0300

    UserController done

--- SocialMedia/src/main/java/com/restful/socialmedia/controller/UserController.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/controller/UserController.java
@@ -1,9 +1,28 @@
 package com.restful.socialmedia.controller;
 
+import com.restful.socialmedia.model.RegistrationRequest;
+import com.restful.socialmedia.model.User;
+import com.restful.socialmedia.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody RegistrationRequest request) {
+        User user = userService.registerUser(request.getUsername(), request.getEmail(), request.getPassword());
+
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }
---
+++ SocialMedia/src/main/java/com/restful/socialmedia/model/RegistrationRequest.java
@@ -0,0 +1,37 @@
+package com.restful.socialmedia.model;
+
+public class RegistrationRequest {
+    private String username;
+    private String email;
+    private String password;
+
+    public RegistrationRequest(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
