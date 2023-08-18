93065c35c7f0aea2b687c0db40dfa064ee38137f
otttisss <otttisss@yandex.ru>
2023-08-18 23:29:51 +0300

    MainClass added | need to test

---
+++ SocialMedia/src/main/java/com/restful/socialmedia/MainClass.java
@@ -0,0 +1,11 @@
+package com.restful.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
