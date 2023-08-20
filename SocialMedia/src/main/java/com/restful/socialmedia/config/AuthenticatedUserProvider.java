4cfed364c9186c218007956d82188990ef4dbb16
otttisss <otttisss@yandex.ru>
2023-08-20 15:32:42 +0300

    AuthenticatedUserProvider updated

--- SocialMedia/src/main/java/com/restful/socialmedia/config/AuthenticatedUserProvider.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/config/AuthenticatedUserProvider.java
@@ -5,14 +5,17 @@ import com.restful.socialmedia.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticatedUserProvider {
     private final UserRepository userRepository;
+    private final UserDetailsService userDetailsService;
 
-    public AuthenticatedUserProvider(UserRepository userRepository) {
+    public AuthenticatedUserProvider(UserRepository userRepository, UserDetailsService userDetailsService) {
         this.userRepository = userRepository;
+        this.userDetailsService = userDetailsService;
     }
 
     public User getUserFromAuthenticatedPrincipal() {
@@ -20,7 +23,8 @@ public class AuthenticatedUserProvider {
         if (authentication.getPrincipal() instanceof UserDetails) {
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-            return userRepository.findByUsername(userDetails.getUsername()).orElse(null);
+//            return userRepository.findByUsername(userDetails.getUsername()).orElse(null);
+            return (User) userDetailsService.loadUserByUsername(userDetails.getUsername());
         }
         return null;
     }
