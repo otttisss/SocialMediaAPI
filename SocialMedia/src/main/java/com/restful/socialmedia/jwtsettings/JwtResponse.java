e0d3ab925478c4bf22268c436c8f893c0ea10dd3
otttisss <otttisss@yandex.ru>
2023-08-20 16:04:02 +0300

    JwtResponse created

---
+++ SocialMedia/src/main/java/com/restful/socialmedia/jwtsettings/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.restful.socialmedia.jwtsettings;
+
+public class JwtResponse {
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
