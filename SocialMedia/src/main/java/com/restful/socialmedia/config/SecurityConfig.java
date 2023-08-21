19e5903458fa162a0cc9c4a7f163854141d6565c
otttisss <otttisss@yandex.ru>
2023-08-21 17:35:57 +0300

    Swagger now works clean! I dont know why, but SWAGGER2 shows WhiteLabel

--- SocialMedia/src/main/java/com/restful/socialmedia/config/SecurityConfig.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/auth/login").permitAll()
                 .anyRequest().permitAll()
                 .and()
--- SocialMedia/src/main/java/com/restful/socialmedia/config/SwaggerConfig.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/config/SwaggerConfig.java
@@ -1,22 +1,24 @@
 package com.restful.socialmedia.config;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// url http://localhost:8080/swagger-ui/index.html
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.restful.socialmedia.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+@OpenAPIDefinition (
+        info = @Info (
+                title = "Social Media API",
+                version = "1.0",
+                description = "Implementation of RESTful API",
+                contact = @Contact(
+                        name = "Efim",
+                        email = "efimotis@gmail.com"
+                )
+        )
+)
+public class SwaggerConfig implements WebMvcConfigurer {
+
 }
