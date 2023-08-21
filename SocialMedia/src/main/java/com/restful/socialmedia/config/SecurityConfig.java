572b130f3dae194f0dbad7bfe659382dde943b70
otttisss <otttisss@yandex.ru>
2023-08-21 14:30:56 +0300

    authenticationManagerBean created | need to test

--- SocialMedia/src/main/java/com/restful/socialmedia/config/SecurityConfig.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -51,9 +52,8 @@ public class SecurityConfig extends WebSecurityConfiguration {
         return new InMemoryUserDetailsManager(user);
     }
 
-    @Override
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
