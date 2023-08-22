d3108e6baa48337026563338312e0ef0d4f5f9d1
otttisss <otttisss@yandex.ru>
2023-08-22 17:59:58 +0300

    FriendRequestController updated | Unit test need to add

--- SocialMedia/src/main/java/com/restful/socialmedia/controller/FriendRequestController.java
+++ SocialMedia/src/main/java/com/restful/socialmedia/controller/FriendRequestController.java
@@ -16,19 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/friend-request")
 public class FriendRequestController {
     private final FriendRequestService friendRequestService;
-//    private final AuthenticatedUserProvider authenticatedUserProvider;
     private final UserService userService;
 
     public FriendRequestController(FriendRequestService friendRequestService, UserService userService) {
         this.friendRequestService = friendRequestService;
-//        this.authenticatedUserProvider = authenticatedUserProvider;
         this.userService = userService;
     }
 
     @PostMapping("/send")
     public ResponseEntity<Friends> sendFriendRequest(@RequestBody FriendsRequests requests) {
-//        User sender = authenticatedUserProvider.getUserFromAuthenticatedPrincipal();
-//        User receiver = userService.getUserByName(requests.getUsername());
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
